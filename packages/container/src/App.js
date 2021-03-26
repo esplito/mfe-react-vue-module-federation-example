ddc37cced96effa311a8fe3bea116aca7fe28ec9
Emil Esplund <eesplund@gmail.com>
2021-03-26 13:01:02 +0100

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
