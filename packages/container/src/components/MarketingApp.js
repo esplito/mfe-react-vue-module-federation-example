87b2c8e452a565af57c2772bf0fc468186bd54b9
Emil Esplund <eesplund@gmail.com>
2021-03-28 10:50:13 +0200

    fixed navigation issues in container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,24 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // you should be able to the same kind of pattern in other frameworks like Angular or Vue.
 export default () => {
   const ref = useRef(null);
+  const history = useHistory(); // copy of the browser history within container
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      }
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'; // react uses this for routing
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate); // this will listen to any changes in navigation
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      // console.log('Container just navigated');
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 // If we are in development and in isolation, call mount immediately
@@ -12,7 +30,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() }); // this will make navigation use browser history when we run the marketing app in isolation
   }
 }
 
